cc26ddd678bdca45d792c232c6b822ff58d7bc7e
kamalcr7 <kamalraj05@gmail.com>
2020-09-03 21:23:09 +0800

    Update main.java

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ class test{
     public static void main(String[] args) {
         System.out.print("Hello World");
         System.out.print("Hello World");
+        System.out.print("Hello World");
     }
 }

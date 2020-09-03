cf256cc2a5fcd0845b792f86f73c52203d67d6c9
zetlaska <45538656+zetlaska@users.noreply.github.com>
2020-09-03 21:44:20 +0800

    Create test2.java

---
+++ src/test2.java
@@ -0,0 +1,8 @@
+class test{
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+    }
+}
